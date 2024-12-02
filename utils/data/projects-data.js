1241efed15777b2e102faf9026cce629cc1f7d75
MyCodeWithSADDY <wuzzydetective@gmail.com>
2024-12-02 17:29:11 +0500

    Update projects-data.js

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -5,7 +5,7 @@ import travel from '/public/image/travel.jpg';
 
 export const projectsData = [
     {
-        id: 1,
+        id: 2,
         name: 'Insta Chow',
         description: "I have Created an App both backend and frontend of it using react native, recieve and take orders from REST API calls and it has a live chat service with the help of Socket.io which was connected to nonrelational data aka MongoDB",
         tools: ['React Native Community CLI', 'Socket.io', 'Fastify', 'Visual Studio', 'Git', 'MongoDB'],
@@ -15,10 +15,10 @@ export const projectsData = [
         image: crefin,
     },
     {
-        id: 2,
-        name: 'Image Library',
-        description: 'as a mern stack developer, I have developed an image library which allows you to fetch images, In this project, I have used axios to fetch images from database and send the data to the frontend where a user can easily access them',
-        tools: ['React', 'Material UI', "Tailwind", "Bootstrap", "Axios", "Visual Studio", "Git", "MongoDB", "express"],
+        id: 1,
+        name: 'IMS Chat App',
+        description: 'as a mern stack developer, I have developed an Web Chat Application in this project, This Application allows you to have real time Chat with your friends, send/recieve images, videos, audio or any files to your friends, make Groups with your friends, send Friend Requests, recieve RealTime notifications all of this with smooth animations',
+        tools: ['React', 'Material UI', "MongoDB", "Express", "Socket.io", "Cloudinary", "Axios", "styled Components", "react-redux/Redux-ToolKit", "NodeJs"],
         role: 'Web Developer',
         code: '',
         demo: '',
